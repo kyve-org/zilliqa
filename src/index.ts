34d4830238f7cd7abd5ff423c0d630fb4671e8de
Troy Kessler <troy.kessler99@gmail.com>
2022-07-13 18:35:31 +0200

    finished zilliqa

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,10 @@
-import KYVE, { Item } from "@kyve/core";
-import { fetchBlock, fetchHeight } from "./utils";
-import { name, version } from "../package.json";
+import { Node, Arweave, Gzip, JsonFileCache } from "@kyve/core";
 
-process.env.KYVE_RUNTIME = name;
-process.env.KYVE_VERSION = version;
+import Zilliqa from "./runtime";
 
-KYVE.metrics.register.setDefaultLabels({
-  app: process.env.KYVE_RUNTIME,
-});
-
-class KyveZilliqa extends KYVE {
-  public async getDataItem(key: string): Promise<Item> {
-    let block;
-
-    const height = await fetchHeight(this.pool.config.rpc);
-    if (+key > height) throw new Error();
-
-    try {
-      block = await fetchBlock(this.pool.config.rpc, +key);
-    } catch (err) {
-      this.logger.warn(`Failed to fetch block ${key}. Retrying ...`);
-
-      throw err;
-    }
-
-    return { key, value: block };
-  }
-
-  public async getNextKey(key: string): Promise<string> {
-    if (key) {
-      return (parseInt(key) + 1).toString();
-    }
-
-    return "0";
-  }
-
-  public async formatValue(value: any): Promise<string> {
-    return value.body?.BlockHash ?? "hash";
-  }
-}
-
-// noinspection JSIgnoredPromiseFromCall
-new KyveZilliqa().start();
+new Node()
+  .addRuntime(new Zilliqa())
+  .addStorageProvider(new Arweave())
+  .addCompression(new Gzip())
+  .addCache(new JsonFileCache())
+  .start();
---
+++ src/runtime.ts
@@ -0,0 +1,31 @@
+import { DataItem, IRuntime, Node } from "@kyve/core";
+import { name, version } from "../package.json";
+import { fetchBlock, fetchHeight } from "./utils";
+
+export default class Zilliqa implements IRuntime {
+  public name = name;
+  public version = version;
+
+  public async getDataItem(core: Node, key: string): Promise<DataItem> {
+    let block;
+
+    const height = await fetchHeight(core.poolConfig.rpc);
+    if (+key > height) throw new Error();
+
+    try {
+      block = await fetchBlock(core.poolConfig.rpc, +key);
+    } catch (err) {
+      throw err;
+    }
+
+    return { key, value: block };
+  }
+
+  public async getNextKey(key: string): Promise<string> {
+    return (parseInt(key) + 1).toString();
+  }
+
+  public async formatValue(value: any): Promise<string> {
+    return value.hash;
+  }
+}
